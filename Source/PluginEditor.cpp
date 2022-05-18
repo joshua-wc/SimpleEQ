6cf8aa6e611a03470510c819e8be0d41604314bc
Joshua Worthington-Church <joshkosh99@gmail.com>
2022-05-18 16:34:51 +1200

    Cleanup

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -112,7 +112,6 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 {
-    //return getLocalBounds();
     auto bounds = getLocalBounds();
 
     auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
@@ -306,9 +305,9 @@ void ResponseCurveComponent::updateChain()
 
         Array<float> freqs
         {
-            20, /*30, 40,*/ 50, 100,
-            200, /*300, 400,*/ 500, 1000,
-            2000, /*3000, 4000,*/ 5000, 10000,
+            20,50, 100,
+            200,500, 1000,
+            2000,5000, 10000,
             20000
         };
 
@@ -329,9 +328,6 @@ void ResponseCurveComponent::updateChain()
         g.setColour(Colours::dimgrey);
         for (auto x : xs)
         {
-            //auto normX = mapFromLog10(f, 20.f, 20000.f);
-
-            //g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
             g.drawVerticalLine(x, top, bottom);
         }
 
@@ -343,12 +339,9 @@ void ResponseCurveComponent::updateChain()
         for (auto gDb : gain)
         {
             auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-            //g.drawHorizontalLine(y, 0, getWidth());
             g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey);
             g.drawHorizontalLine(y, left, right);
         }
-
-        // g.drawRect(getAnalysisArea());
 
         g.setColour(Colours::lightgrey);
         const int fontHeight = 10;
@@ -401,6 +394,18 @@ void ResponseCurveComponent::updateChain()
 
                 g.drawFittedText(str, r, juce::Justification::centred, 1);
 
+                str.clear();
+                str << (gDb - 24.f);
+
+                r.setX(1);
+                textWidth = g.getCurrentFont().getStringWidth(str);
+                r.setSize(textWidth, fontHeight);
+                g.setColour(Colours::lightgrey);
+
+                g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+
+
 
             }
         }
@@ -410,8 +415,6 @@ void ResponseCurveComponent::updateChain()
     juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
     {
         auto bounds = getLocalBounds();
-
-        //bounds.reduce(10, 8);
 
         bounds.removeFromTop(12);
         bounds.removeFromBottom(2);
@@ -507,7 +510,7 @@ void SimpleEQAudioProcessorEditor::resized()
     // subcomponents in your editor..
 
     auto bounds = getLocalBounds();
-    float hRatio = 25.f / 100.f; // JUCE_LIVE_CONSTANT(33) / 100.f;
+    float hRatio = 25.f / 100.f;
     auto responseArea = bounds.removeFromTop(bounds.getHeight() * hRatio);
 
     responseCurveComponent.setBounds(responseArea);
